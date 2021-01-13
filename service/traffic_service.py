060432dd4233238b52d54f4f6b491ad8c94533a4
Maxime Girma <maxime.girma@hotmail.fr>
2021-01-13 16:19:35 +0100

    wip

--- service/traffic_service.py
+++ service/traffic_service.py
@@ -6,7 +6,7 @@ from service.traffic_forecast import TrafficForecast
 
 def get_forecast_by_hour(selected_hour: str) -> TrafficForecast:
     """Return a Forecast object for a given hour.
-    TODO: eash call to the method call the api, this can be optimized
+    TODO: each call to the method call the api, this can be optimized
     """
     forecasts_list = get_all_traffic_forecasts()
     for forecast in forecasts_list:
@@ -19,7 +19,7 @@ Metric = namedtuple("Metric", ["minimum", "maximum", "average"])
 
 def get_metrics() -> Metric:
     forecasts_list = get_all_traffic_forecasts()
-    minimum = min([forecast.vehicles_count for forecast in forecasts_list])
+    minimum = min(forecasts_list)
     maximum = max([forecast.vehicles_count for forecast in forecasts_list])
     average = sum([forecast.vehicles_count for forecast in forecasts_list]) / len(
         forecasts_list
