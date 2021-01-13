49009027523b586321bc9fd98e7096868ff0c6ee
Maxime Girma <maxime.girma@hotmail.fr>
2021-01-13 15:24:12 +0100

    basic metrics for the day

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from math import ceil
+
 from service.traffic_api import get_all_traffic_forecasts
-from service.traffic_service import get_forecast_by_hour
+from service.traffic_service import get_forecast_by_hour, get_metrics
 
 
 def main():
     hour = input("please enter an hour : ")
     forecast = get_forecast_by_hour(str(hour))
     print(f"Prevision for the hour is {forecast.vehicles_count}.")
-
+    metrics = get_metrics()
+    print(f"minimum for today is : {metrics['minimum']}")
+    print(f"maximum for today is : {metrics['maximum']}")
+    print(f"average for today is : {ceil(metrics['average'])}")
 
 
 if __name__ == "__main__":
--- service/traffic_service.py
+++ service/traffic_service.py
@@ -7,3 +7,17 @@ def get_forecast_by_hour(selected_hour: str) -> TrafficForecast:
     for forecast in forecasts_list:
         if str(forecast.timestamp.hour) == selected_hour:
             return forecast
+
+
+def get_metrics():
+    forecasts_list = get_all_traffic_forecasts()
+    minimum = min([forecast.vehicles_count for forecast in forecasts_list])
+    maximum = max([forecast.vehicles_count for forecast in forecasts_list])
+    average = sum([forecast.vehicles_count for forecast in forecasts_list]) / len(
+        forecasts_list
+    )
+    return {
+        "minimum": minimum,
+        "maximum": maximum,
+        "average": average,
+    }
